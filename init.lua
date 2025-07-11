0013b762a4dd29de61d6922afef78fb110faf7e1
Dominic O'Donnell <93290186+Dominicod@users.noreply.github.com>
2025-07-11 18:43:10 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -80,6 +80,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<C-E>', '<cmd>Neotree reveal<enter>', { desc = 'Open Neotree' })
+vim.keymap.set('n', 'zm', '<cmd>ZenMode<enter>', { desc = 'Enters ZenMode' })
 
 -- Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -138,6 +139,14 @@ require('lazy').setup({
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = true,
+  },
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      window = {
+        width = 240,
+      },
+    },
   },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
